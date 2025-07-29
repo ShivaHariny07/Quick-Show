253730c99af31603c0df4eb5fb8a3b916e1b257e
ShivaHariny07 <shivahariny07@gmail.com>
2025-07-30 00:47:58 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,8 +8,9 @@ const Footer = () => {
                 <div className="md:max-w-96">
                     <img className="w-36 h-auto" src={assets.logo} alt="logo" />
                     <p className="mt-6 text-sm">
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </p>
+QuickShow is an online movie ticket booking platform that lets users explore movies, select seats, and pay securely.
+It offers user login, booking history, and a smooth ticketing experience with real-time seat availability.
+Admins can manage shows and bookings easily through a dedicated dashboard.                    </p>
                     <div className="flex items-center gap-2 mt-4">
                         <img src={assets.googlePlay} alt="google play" className="h-9 w-auto" />
                         <img src={assets.appStore} alt="app store" className="h-9 w-auto" />
@@ -35,7 +36,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="pt-4 text-center text-sm pb-5">
-                Copyright {new Date().getFullYear()} © GreatStack. All Right Reserved.
+                Copyright {new Date().getFullYear()} © Quick Show. All Right Reserved.
             </p>
         </footer>
   )
